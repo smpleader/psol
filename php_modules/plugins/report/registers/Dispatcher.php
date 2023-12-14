1995d419535a27cd723de7552cf5bd202af090be
huylq.hust <lqhuy.hust@gmail.com>
2023-12-14 17:42:28 +0700

    add logic check login when access page request

--- php_modules/plugins/report/registers/Dispatcher.php
+++ php_modules/plugins/report/registers/Dispatcher.php
@@ -9,6 +9,9 @@ class Dispatcher
 {
     public static function dispatch(IApp $app)
     {
+        // Check Permission
+        $app->plgLoad('permission', 'CheckSession');
+        
         $reportDispatcher = new ReportDispatch($app->getContainer());
         $reportDispatcher->execute();
     }
