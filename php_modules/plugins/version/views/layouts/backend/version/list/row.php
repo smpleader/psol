2c19503136b115c87a245e10787149ddf3921335
huylq.hust <lqhuy.hust@gmail.com>
2023-12-14 18:21:22 +0700

    show short text of change logs page versions

--- php_modules/plugins/version/views/layouts/backend/version/list/row.php
+++ php_modules/plugins/version/views/layouts/backend/version/list/row.php
@@ -24,8 +24,25 @@
     </td>
     <td>
         <?php 
-        if($this->get_log){
-            foreach($this->get_log as $log)  {
+        $logs = $this->get_log;
+        if(count($logs) > 3){
+            $logs_list = '<ul>';
+            foreach($logs as $log)  {
+                if($log['version_id'] == $this->item['id']){
+                    $logs_list .= '<li>' . $log['log'] . '</li>';
+                }
+            }
+            $logs_list .= '</ul>';
+ 
+            echo '<a href="#" class="show_data" data-logs="' . $logs_list . '" data-name="' . item['name'] .'" data-bs-placement="top" data-bs-toggle="modal" data-bs-target="#showChangeLogsModal">';
+            for ($i=0;$i<3;$i++) {
+                if($logs[$i]['version_id'] == $this->item['id']){
+                    echo '<span>'. '- ' . $logs[$i]['log'] . '</span> <br>'; 
+                }
+            }
+            echo '...';
+        } else {
+            foreach($logs as $log)  {
                 if($log['version_id'] == $this->item['id']){
                     echo '<span>'. '- ' . $log['log'] . '</span> <br>'; 
                 }
@@ -41,3 +58,29 @@
         <a class="fs-4 me-1" href="<?php echo $this->link_form . '-feedback/' . $this->item['id']; ?>" data-bs-toggle="tooltip" data-bs-placement="top" title="Go to feedback"><i class="fa-solid fa-message"></i></a>
     </td>
 </tr>
+
+<div class="modal fade" id="showChangeLogsModal" aria-labelledby="showChangeLogsModal" tabindex="-1" aria-hidden="true" style="display: none;">
+    <div class="modal-dialog modal-dialog-centered " style="max-width: 600px;">
+        <div class="modal-content container px-5 pt-5">
+                <div class="row g-3 align-items-center">
+                    <div class="row">
+                        <div class="mb-3 col-12 mx-auto pt-3">
+                            <h2></h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col-12 mx-auto">
+                            <div class="row">
+                                <div class="col-3 d-flex align-items-center">
+                                    <label class="form-label fw-bold mb-0">Release Date</label>
+                                </div>
+                                <div class="col-9">
+                                    <?php $this->ui->field('release_date'); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
